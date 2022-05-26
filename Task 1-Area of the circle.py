62f1f30cadd9a270800a14b91fdfa73065d17a11
Raima Roj <65860814+raima2000@users.noreply.github.com>
2022-05-26 16:07:55 +0530

    Add files via upload

---
+++ Task 1-Area of the circle.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 26 14:43:29 2022
+
+@author: Raima
+"""
+
+pi = 3.14
+Radius = float(input("Input the Radius of the Circle:"))
+area_circle = pi*Radius*Radius
+print("Area of the Circle with radius 1.1 is:",area_circle)
