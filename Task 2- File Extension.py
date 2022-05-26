02c7c1b21c504006a20548589070af4466dbdd43
Raima Roj <65860814+raima2000@users.noreply.github.com>
2022-05-26 16:49:46 +0530

    Add files via upload

---
+++ Task 2- File Extension.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 26 16:21:14 2022
+
+@author: Raima
+"""
+
+file_name = input("Input the Filename:")
+file_extension = file_name.split('.')
+print("The extension of the File is:",file_extension[-1])
