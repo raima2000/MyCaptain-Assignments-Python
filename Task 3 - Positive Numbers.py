a69b6b5c3b4776f332ab9b829f9e22899f7f2ac7
Raima Roj <65860814+raima2000@users.noreply.github.com>
2022-05-26 17:26:38 +0530

    Add files via upload

---
+++ Task 3 - Positive Numbers.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 26 17:13:49 2022
+
+@author: Raima
+"""
+
+L1 = [12,-10,3,5,44,-67,-81,21,9]
+for a in L1:
+    if a >= 0:
+        print(a , end = " ")
+        
+L2 = [22,13, -7,-45]  
+for b in L2:
+    if b >= 0:
+        print(b, end =" ")
+      
